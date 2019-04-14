b32d300c8bed9b64cbc7f2a446a81f8fb831e5a1
周厚铨 <fuhrer.zhou@gmail.com>
2019-04-14 17:36:53 +0800

    Change test parameters

--- test/test_search.cpp
+++ test/test_search.cpp
@@ -13,19 +13,19 @@ using namespace GOMOKUZHQ;
 
 TEST(search, c1) {
     const int size = 15;
-    GameBoard game_board(size);
     int board[size][size];
-    memset(board, 0, sizeof(board));
 
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(size / 4 - 2, 3 * size / 4 + 2);
 
     float total_time = 0;
-    int TIMES = 1;
+    int TIMES = 50;
     for (int t = 0;t < TIMES;++t) {
+        GameBoard game_board(size);
+        memset(board, 0, size * size * sizeof(int));
         int i, j;
-        for (int k = 0; k < 15; ++k) {
+        for (int k = 0; k < 8; ++k) {
             while (true) {
                 i = dis(gen);
                 j = dis(gen);
@@ -45,9 +45,11 @@ TEST(search, c1) {
                 }
             }
         }
-        game_board.print();
 
-        GomokuAI ai(12);
+        GomokuAI ai(16);
+        game_board.print();
+        printf("Score: %f\n", ai.state_evaluate(game_board));
+
         auto start_time = high_resolution_clock::now();
         auto res = ai.search(game_board, 1, -numeric_limits<float>::infinity(), numeric_limits<float>::infinity());
         printf("position:(%d, %d)\tscore: %f\n", res.first.first+1, res.first.second+1, res.second);
